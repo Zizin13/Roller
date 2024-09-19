72b893004100c60c6f1932bae9cc9850a861c982
Zizin13 <thespengo@gmail.com>
2024-09-18 22:13:22 -0700

    WhipLib can now be built as a DLL

---
+++ WhipLib/WhipLib.cpp
@@ -0,0 +1,121 @@
+#include "WhipLib.h"
+#include "Palette.h"
+#include "Texture.h"
+#include "ShapeData.h"
+#include "ShapeFactory.h"
+//-------------------------------------------------------------------------------------------------
+#if defined(_DEBUG) && defined(IS_WINDOWS)
+#define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
+#endif
+//-------------------------------------------------------------------------------------------------
+
+WLFUNC int wlLoadTexture(uint8 *pDataBuf, int iBufSize, const char *szTex, const char *szPal)
+{
+  int iRet = -1;
+  if (!pDataBuf || !szTex || !szPal)
+    return iRet;
+
+  //load palette
+  CPalette pal;
+  if (!pal.LoadPalette(szPal))
+    return iRet;
+
+  //load texture
+  CTexture tex;
+  if (!tex.LoadTexture(szTex, &pal))
+    return iRet;
+
+  //generate bmp
+  int iBmpSize;
+  uint8 *pBmpData = tex.GenerateBitmapData(iBmpSize);
+
+  //fill buffer
+  if (iBmpSize <= iBufSize) {
+    iRet = iBmpSize;
+    for (int i = 0; i < iBmpSize; ++i) {
+      pDataBuf[i] = pBmpData[i];
+    }
+  }
+
+  //cleanup
+  delete[] pBmpData;
+
+  return iRet;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+WLFUNC bool wlGetModel(uint8 *pBmpBuf,
+                       int iBmpBufSize,
+                       tWhiplashVertex *pVertexBuf,
+                       int iVertexBufSize,
+                       uint32 *pIndexBuf,
+                       int iIndexBufSize,
+                       eWhipModel model,
+                       const char *szTex,
+                       const char *szPal,
+                       int &iBmpSize,
+                       int &iNumVertices,
+                       int &iNumIndices)
+{
+  bool bSuccess = false;
+  if (!pBmpBuf || !pVertexBuf || !pIndexBuf || !szTex || !szPal)
+    return bSuccess;
+
+  CShapeFactory::GetShapeFactory().m_bOglRunning = false;
+
+  //load palette
+  CPalette pal;
+  if (!pal.LoadPalette(szPal))
+    return bSuccess;
+
+  //load texture
+  CTexture tex;
+  if (!tex.LoadTexture(szTex, &pal))
+    return bSuccess;
+
+  //generate bmp
+  uint8 *pBmpData = tex.GenerateBitmapData(iBmpSize);
+
+  //generate model
+  CShapeData *pShape = CShapeFactory::GetShapeFactory().MakeModel(NULL, &tex, model);
+  if (!pShape)
+    return bSuccess;
+
+  //fill buffers
+  if (pShape->m_uiNumVerts <= (uint32)(iVertexBufSize / sizeof(tWhiplashVertex))
+      && pShape->m_uiNumIndices <= (uint32)iIndexBufSize / sizeof(uint32)
+      && iBmpSize <= iBmpBufSize)
+  {
+    bSuccess = true;
+    iNumVertices = (int)pShape->m_uiNumVerts;
+    iNumIndices = (int)pShape->m_uiNumIndices;
+    for (int i = 0; i < iBmpSize; ++i) {
+      pBmpBuf[i] = pBmpData[i];
+    }
+    for (int i = 0; i < (int)pShape->m_uiNumVerts; ++i) {
+      pVertexBuf[i].fX = pShape->m_vertices[i].position.x;
+      pVertexBuf[i].fY = pShape->m_vertices[i].position.y;
+      pVertexBuf[i].fZ = pShape->m_vertices[i].position.z;
+      pVertexBuf[i].fNormalX = pShape->m_vertices[i].normal.x;
+      pVertexBuf[i].fNormalY = pShape->m_vertices[i].normal.y;
+      pVertexBuf[i].fNormalZ = pShape->m_vertices[i].normal.z;
+      pVertexBuf[i].fColorR = pShape->m_vertices[i].color.r;
+      pVertexBuf[i].fColorB = pShape->m_vertices[i].color.b;
+      pVertexBuf[i].fColorG = pShape->m_vertices[i].color.g;
+      pVertexBuf[i].fColorA = pShape->m_vertices[i].color.a;
+      pVertexBuf[i].bUseColor = pShape->m_vertices[i].byUseColor == 1;
+    }
+    for (int i = 0; i < (int)pShape->m_uiNumIndices; ++i) {
+      pIndexBuf[i] = pShape->m_indices[i];
+    }
+  }
+
+  //cleanup
+  delete pShape;
+  delete[] pBmpData;
+
+  return bSuccess;
+}
+
+//-------------------------------------------------------------------------------------------------
---
+++ WhipLib/WhipLib.h
@@ -0,0 +1,91 @@
+#ifndef _WHIPLIB_WHIPLIB_H
+#define _WHIPLIB_WHIPLIB_H
+//-------------------------------------------------------------------------------------------------
+#include "Types.h"
+//-------------------------------------------------------------------------------------------------
+
+#if defined(WIN32) || defined(_WIN32)
+  #if defined(WHIPLIB_LIB)
+    #define WLFUNC extern "C"
+  #elif defined(WHIPLIB_DLL)
+    #define WLFUNC extern "C" __declspec(dllexport)
+  #else
+    #define WLFUNC extern "C" __declspec(dllimport)
+  #endif
+  #define WLCDECL __cdecl
+#else
+  #define WLFUNC extern "C"
+  #define WLCDECL
+#endif
+
+//-------------------------------------------------------------------------------------------------
+
+struct tWhiplashVertex
+{
+  tWhiplashVertex()
+    : fX(0.0f)
+    , fY(0.0f)
+    , fZ(0.0f)
+    , fNormalX(0.0f)
+    , fNormalY(0.0f)
+    , fNormalZ(0.0f)
+    , fColorR(0.0f)
+    , fColorB(0.0f)
+    , fColorG(0.0f)
+    , fColorA(0.0f)
+    , bUseColor(false)
+  {};
+
+  float fX;
+  float fY;
+  float fZ;
+  float fNormalX;
+  float fNormalY;
+  float fNormalZ;
+  float fColorR;
+  float fColorB;
+  float fColorG;
+  float fColorA;
+  bool bUseColor;
+};
+
+//-------------------------------------------------------------------------------------------------
+
+// Loads a Whiplash texture file and fills pDataBuf with a bitmap conversion including file header
+// Returns the size of the bitmap or -1 if there is an error
+WLFUNC int wlLoadTexture(uint8 *pDataBuf, int iBufSize, const char *szTex, const char *szPal);
+typedef int (WLCDECL *wlLoadTextureFunc)(uint8 *pDataBuf, int iBufSize, const char *szTex, const char *szPal);
+
+
+// Fills pBmpBuf with a bitmap conversion of the texture and palette files including file header,
+//   pVertexBuf with vertices, and pIndexBuf with indices for the chosen model.
+// iBmpSize will be set to the size of the bitmap file
+// iNumVertices and iNumIndices will be set to the number of vertices and indices
+// Returns true if successful
+WLFUNC bool wlGetModel(uint8 *pBmpBuf,
+                       int iBmpBufSize,
+                       tWhiplashVertex *pVertexBuf,
+                       int iVertexBufSize,
+                       uint32 *pIndexBuf,
+                       int iIndexBufSize,
+                       eWhipModel model,
+                       const char *szTex,
+                       const char *szPal,
+                       int &iBmpSize,
+                       int &iNumVertices,
+                       int &iNumIndices);
+typedef bool (WLCDECL *wlGetModelFunc)(uint8 *pBmpBuf,
+                                       int iBmpBufSize,
+                                       tWhiplashVertex *pVertexBuf,
+                                       int iVertexBufSize,
+                                       uint32 *pIndexBuf,
+                                       int iIndexBufSize,
+                                       eWhipModel model,
+                                       const char *szTex,
+                                       const char *szPal,
+                                       int &iBmpSize,
+                                       int &iNumVertices,
+                                       int &iNumIndices);
+
+//-------------------------------------------------------------------------------------------------
+#endif

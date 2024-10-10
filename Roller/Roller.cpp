d2a7863ea184a8656421bd4c6b35c54a13535a64
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2024-10-10 13:29:21 -0700

    adding glfw

--- Roller/Roller.cpp
+++ Roller/Roller.cpp
@@ -1,4 +1,8 @@
 //-------------------------------------------------------------------------------------------------
+#define GLFW_INCLUDE_NONE
+#include <glfw3.h>
+#include <glew.h>
+//-------------------------------------------------------------------------------------------------
 #if defined(_DEBUG) && defined(IS_WINDOWS)
 #define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
 #endif
@@ -10,6 +14,34 @@ int main(int argc, char *argv[])
   _set_error_mode(_OUT_TO_MSGBOX);
   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+  GLFWwindow *window;
+
+  if (!glfwInit())
+    return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window)) {
+      /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 
   return 0;
 };

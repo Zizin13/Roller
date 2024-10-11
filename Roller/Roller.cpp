8bf11cb3d58a8320012bf19c3dd1590898ac2fc4
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2024-10-11 15:28:39 -0700

    added logging, window handles resizing

--- Roller/Roller.cpp
+++ Roller/Roller.cpp
@@ -1,10 +1,13 @@
 //-------------------------------------------------------------------------------------------------
 #define GLFW_INCLUDE_NONE
+#include <windows.h>
 #include <glfw3.h>
 #include <glew.h>
 #include <glm.hpp>
 #include "gtc/matrix_transform.hpp"
 #include "gtx/transform.hpp"
+#include "OpenGLDebug.h"
+#include "Logging.h"
 #include "Shader.h"
 #include "Camera.h"
 #include "ShapeFactory.h"
@@ -15,6 +18,15 @@
 #if defined(_DEBUG) && defined(IS_WINDOWS)
 #define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
 #endif
+//-------------------------------------------------------------------------------------------------
+
+static void LogMessageCbStatic(const char *szMsg, int iLen)
+{
+  (void)(iLen);
+  OutputDebugString(szMsg);
+  OutputDebugString("\n");
+}
+
 //-------------------------------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
@@ -28,20 +40,22 @@ int main(int argc, char *argv[])
   if (!glfwInit())
     return -1;
 
-  /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  //Create a windowed mode window and its OpenGL context
+  window = glfwCreateWindow(640, 480, "Roller", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
   }
 
-  /* Make the window's context current */
+  Logging::SetWhipLibLoggingCallback(LogMessageCbStatic);
+
+  //Make the window's context current
   glfwMakeContextCurrent(window);
 
   if (glewInit() != GLEW_OK)
     return -1;
   glEnable(GL_DEBUG_OUTPUT);
-  //glDebugMessageCallback(GLErrorCb, 0);
+  glDebugMessageCallback(GLErrorCb, 0);
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_CULL_FACE);
   glEnable(GL_BLEND);
@@ -50,6 +64,7 @@ int main(int argc, char *argv[])
   glAlphaFunc(GL_GREATER, 0.24f);
   glLineWidth(3.0f);
   CShader shader("Shaders/WhiplashVertexShader.glsl", "Shaders/WhiplashFragmentShader.glsl");
+
   Camera camera;
   camera.m_position.z = 500.0f;
   camera.m_position.y = 1300.0f;
@@ -61,26 +76,27 @@ int main(int argc, char *argv[])
   pTex->LoadTexture("C:\\WHIP\\WHIPLASH\\FATDATA\\YZIZIN.BM", pPal);
   CShapeData *pShape = CShapeFactory::GetShapeFactory().MakeModel(&shader, pTex, eWhipModel::CAR_YZIZIN);
 
-  /* Loop until the user closes the window */
+  //Loop until the user closes the window
   while (!glfwWindowShouldClose(window)) {
-    /* Render here */
     glClearColor(0.03125f, 0.296875f, 0.984375f, 1.0f);
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-    //glViewport(0, 0, width(), height());
+    int iWidth, iHeight = 0;
+    glfwGetWindowSize(window, &iWidth, &iHeight);
+    glViewport(0, 0, iWidth, iHeight);
 
-    glm::mat4 viewToProjectionMatrix = glm::perspective(glm::radians(60.0f), ((float)640.0f) / 480.0f, 100.0f, 500000.0f);
+    glm::mat4 viewToProjectionMatrix = glm::perspective(glm::radians(60.0f), ((float)iWidth) / iHeight, 100.0f, 500000.0f);
     glm::mat4 worldToViewMatrix = camera.GetWorldToViewMatrix();
     glm::mat4 worldToProjectionMatrix = viewToProjectionMatrix * worldToViewMatrix;
 
     pShape->Draw(worldToProjectionMatrix, camera.m_position);
 
-    /* Swap front and back buffers */
+    //Swap front and back buffers
     glfwSwapBuffers(window);
-
-    /* Poll for and process events */
+    //Poll for and process events
     glfwPollEvents();
   }
 
+  //cleanup
   delete pShape;
   delete pTex;
   delete pPal;

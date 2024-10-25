befd936c99fcf3d4f6bffbaed7417fd0a7f1bf31
Zizin13 <162662805+Zizin13@users.noreply.github.com>
2024-10-24 20:34:24 -0700

    adding imgui

--- Roller/Roller.cpp
+++ Roller/Roller.cpp
@@ -10,6 +10,9 @@
 #include "GlfwKeyMapper.h"
 #include "Scene.h"
 #include "stb_image.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 //-------------------------------------------------------------------------------------------------
 #if defined(_DEBUG) && defined(IS_WINDOWS)
 #define new new(_CLIENT_BLOCK, __FILE__, __LINE__)
@@ -55,6 +58,27 @@ int main(int argc, char *argv[])
   glfwMakeContextCurrent(pWindow);
   //don't limit framerate, must be done after glfwMakeContextCurrent
   glfwSwapInterval(0);
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO(); (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+  ImGuiStyle &style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(pWindow, true);
+  const char *glsl_version = "#version 130";
+  ImGui_ImplOpenGL3_Init(glsl_version);
   
   //init game
   Logging::SetWhipLibLoggingCallback(LogMessageCbStatic);
@@ -99,6 +123,26 @@ int main(int argc, char *argv[])
     CGameInput::GetGameInput().Update();
     testScene.Update(iWidth, iHeight);
 
+    //imgui
+    if (glfwGetWindowAttrib(pWindow, GLFW_ICONIFIED) != 0) {
+      ImGui_ImplGlfw_Sleep(10);
+      continue;
+    }
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+    ImGui::Begin("Hello, world!");
+    ImGui::Text("This is some useful text.");
+    ImGui::End();
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+      GLFWwindow *backup_current_context = glfwGetCurrentContext();
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+      glfwMakeContextCurrent(backup_current_context);
+    }
+
     //Swap front and back buffers
     glfwSwapBuffers(pWindow);
     //Poll for and process events
@@ -107,6 +151,9 @@ int main(int argc, char *argv[])
 
   //shutdown
   testScene.Shutdown();
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
   glfwTerminate();
 
   return 0;
